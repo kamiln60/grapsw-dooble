97a0b1d27272cd560945d6bdc6832b2ad94d3341
Aleksander Wójcik <aleks4@outlook.com>
2020-07-17 21:02:05 +0200

    Poprawione namspace'y

--- DobbleClient/NewTable.xaml.cs
+++ DobbleClient/NewTable.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GuiDoobleGame
+namespace DobbleClient
 {
     /// <summary>
     /// Logika interakcji dla klasy NewTable.xaml
