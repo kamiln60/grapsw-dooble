42db90cc6bfd5ab4e4b1ee58914699708aeb26a4
Kamiln60 <Kamiln60@gmail.com>
2020-07-17 18:00:55 +0200

    zmiany

---
+++ GuiDoobleGame/Login.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GuiDoobleGame
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Login.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void NewTable(object sender, RoutedEventArgs e)
+        {
+            NewTable nt = new NewTable();
+            this.NavigationService.Navigate(nt);
+        }
+
+
+
+        private void singleplayertable(object sender, RoutedEventArgs e)
+        {
+            singleplayertable sp = new singleplayertable();
+            this.NavigationService.Navigate(sp);
+        }
+
+
+
+        private void SelectTable(object sender, RoutedEventArgs e)
+        {
+            SelectTable st = new SelectTable();
+            this.NavigationService.Navigate(st);
+        }
+
+        private void Help(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
